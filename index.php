3c43876defdc2157de0774dc68b4fadd0b5eb167
Nataliia-Zemlyak <znatashaa13@gmail.com>
2022-06-01 18:06:45 +0300

    tea2

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Заварювання чаю</title>
+</head>
+<body>
+    <form method="POST" action="tea.php">
+            <h2>Оберіть кількість води, мл</h2>
+            <p>
+            <input type="range" name="water" value="50" min="50" max="1000" step="50" oninput="this.nextElementSibling.value =this.value">
+            <input type="text" oninput="this.previousElementSibling.value =this.value">
+            </p>
+
+            <h2>Оберіть кількість цукру</h2>
+            <p>
+            <input type="range" name="shugar" value="0" min="0" max="10" step="0.5" oninput="this.nextElementSibling.value =this.value">
+            <input type="text" oninput="this.previousElementSibling.value =this.value">
+            </p>
+            <input type="submit" value="Замовити">
+
+            <input type="hidden" name="cup" value="0">
+            
+            
+            
+
+
+            
+
+    
+
+    </form>
+</body>
+</html>
---
+++ tea.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Заварювання чаю</title>
+</head>
+<body>
+    
+    <form method="POST" action="tea.php">
+        
+        <?php
+        if ($_POST) {
+            $water = $_POST['water'];
+            $shugar = $_POST['shugar'];
+            $cup = 1;
+    
+
+            $sugarInOneCup = $shugar / $water * 50; //цукор на 50 мл води 
+            $sugarOfCup = 0;
+            
+            
+            echo ("<h2> Ваше замовлення </h2>");
+            echo ("<p>Water ".$water ."</p>Shugar ".$shugar);
+        
+            echo ("<p>Чайник закипів</p>");
+
+            while ($water > 0) { 
+                for ($i = 50; $water > 0 && $i <= 250; $i +=50, $water -= 50, $sugarOfCup += $sugarInOneCup) {
+                    echo ("<p>Налито $i мл води</p>");
+                }
+                    echo ("<p>Насипано ".number_format ($sugarInOneCup, 1)." ч.л. цукру</p>");
+                    $teaInCup = (2/250) * $i;
+                    echo ("<p>Опускаємо чайний пакетик на $teaInCup хв</p>");
+                    echo ("<p>Розмішуємо</p>");
+
+
+                if ($water == 0) {
+                    echo ("<p>Вода закінчилась</p>");
+                } else { 
+                    echo ("<p>Кружка повна</p>");
+
+                }
+                if ($water > 0) {
+                    echo ("беремо наступну кружку");
+                }
+            }
+                echo("<p>Смачного чаювання!</p>");
+        }
+        
+               
+        
+
+    ?>
+    
+    <input type="hidden" name="water" value="<?php echo($water); ?>">
+    <input type="hidden" name="shugar" value="<?php echo($shugar); ?>">
+    
+
+    
+</body>
+</html>
